832c32e4bb666a5b2e114ada5a3bef04dd257fb7
Capril1042 <Caprilewis1042@gmail.com>
2019-04-10 19:32:58 -0400

    PuzzleData gets data from firbase/firestore

--- cryptograms/src/Data/PuzzleData.js
+++ cryptograms/src/Data/PuzzleData.js
@@ -1,3 +1,38 @@
 // Will fetch quotes from the database 
 
+import React, { Component } from 'react';
+import db from '../firebase';
 
+class PuzzleData extends Component {
+    constructor(props) {
+        super(props);
+        this.state ={
+            puzzles:[]
+        }
+    }
+componentDidMount(){
+const quoteRef = db.collection('Quotes');
+let newState = [];
+  quoteRef.get().then((querySnapshot) => {
+    querySnapshot.forEach((doc)=>
+     {
+      console.log(doc.data());
+      newState.push({
+        puzzles: doc.data().Quote,
+      });
+    })
+    this.setState({
+      puzzles: newState
+    });
+
+  });
+}
+
+render () {
+    console.log(this.state.puzzles)
+    return(
+        <div> Puzzles </div>
+    )
+}
+}
+export default PuzzleData;
