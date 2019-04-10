89fdc45cab18e3a516123f182d7e9844cdc3aa19
Capril1042 <Caprilewis1042@gmail.com>
2019-04-09 23:15:31 -0400

    Creates EncodedQuote Function- WIP

--- cryptograms/src/Utils/EncodedQuote.js
+++ cryptograms/src/Utils/EncodedQuote.js
@@ -1,12 +1,28 @@
-// will return original quote and encoded quote
-export default EncodedQuote(quote){
-    // use map to count like characters excluding punctuaion.
-    // map of char-value(count)
-    // count keys of the map
-    // genrate random numbers for count-of-keys
-    //ex. 6chars = 6 random numbers
-    // random numbers = offset
-    // make map of char-offsetchar(if offset = offsetchar = another offset char(use new random number)).
-    // return new quote include orinal spaces and puncution.
+export default function MapOfUniqueLetters(quote){
+let quoteArray = quote.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\s]/g,"").toLowerCase().split("");
+let mapCharToCount = new Map();
+for( let letter in quoteArray)
+  if( !mapCharToCount.has(letter)) {
+  let coded = cb(quoteArray[letter]);
+  mapCharToCount.set(quoteArray[letter],coded);
+}
+coderArray=[97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122];
+return mapCharToCount;
+}
 
+
+  let coderArray= [97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122];
+function cb(letter){
+  let letterCode = letter.charCodeAt(0);
+  let tempIndex = coderArray.indexOf(letterCode);
+  coderArray.splice(tempIndex,1);
+  const rand = getRandomInt(0, coderArray.length-1);
+  let codedLetter = coderArray[rand];
+    coderArray.splice(rand,1);
+    coderArray.splice(tempIndex,0,letterCode);
+    return String.fromCharCode(codedLetter);
+  }
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
