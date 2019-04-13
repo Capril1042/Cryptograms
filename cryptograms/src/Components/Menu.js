4acb66923b47d887065c729316bf8bda6022b200
Capril1042 <Caprilewis1042@gmail.com>
2019-04-13 00:17:12 -0400

    Menu styled, orgnazied

--- cryptograms/src/Components/Menu.js
+++ cryptograms/src/Components/Menu.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import db from '../firebase';
+
+import './Menu.css';
+
 import SubMenu from './SubMenu';
-import categorize from '../Utils/PuzzleDataCategories';
-import puzzlesByCategory from '../Utils/SortedQuotes';
+
 
 class Menu extends Component {
     constructor(props) {
@@ -15,26 +16,28 @@ class Menu extends Component {
 
 handleCategoryClick=(e)=>{
     e.preventDefault();
-this.setState({
-    filter: e.target.value,
+    this.setState({
+        filter: e.target.value,
    })
 }
 
 
 
 render () {
-
-  
     return(
         <div> 
-        Menu
-        <button value="movie" onClick={this.handleCategoryClick}>Movies</button>
-        <button value="music" onClick={this.handleCategoryClick}>Music</button>
-        <SubMenu filter={this.state.filter}/>
-         </div>
-        
-    )
-
-}
+            <div classnames="CategoryTabs">
+                <button value="movie" onClick={this.handleCategoryClick} classname="Category">Movies</button>
+                <button value="music" onClick={this.handleCategoryClick} classname="Category">Music</button>
+                <button value="tv" onClick={this.handleCategoryClick} classname="Category">TV</button>
+                <button value="books" onClick={this.handleCategoryClick} classname="Category">Books</button>
+                <button value="history" onClick={this.handleCategoryClick} classname="Category">History</button>
+            </div>
+            <div className="SubMenu">
+            <SubMenu filter={this.state.filter}/>
+            </div>
+         </div>  
+        )
+    }
 }
 export default Menu;
